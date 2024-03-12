d94afeeb92f8e8808075c01d52cb20fbccc0d6eb
kappaxbeta <jens.boesche@me.com>
2024-03-12 14:24:08 +0100

    Initial commit

---
+++ src/worlds/Starter.tsx
@@ -0,0 +1,24 @@
+// @ts-ignore
+import { Spinning, Floating, StandardReality } from "spacesvr";
+import TransparentFloor from "ideas/TransparentFloor";
+import CloudySky from "ideas/CloudySky";
+
+export default function Starter() {
+    return (
+    <StandardReality>
+      <ambientLight />
+      <group position={[0, 0, -4]}>
+        <Floating>
+          <Spinning xSpeed={0.2} ySpeed={0.4} zSpeed={0.3}>
+            <mesh>
+              <torusKnotBufferGeometry args={[1, 0.2]} />
+              <meshStandardMaterial color="blue" />
+            </mesh>
+          </Spinning>
+        </Floating>
+      </group>
+
+      <TransparentFloor opacity={0.7} />
+    </StandardReality>
+  );
+}
